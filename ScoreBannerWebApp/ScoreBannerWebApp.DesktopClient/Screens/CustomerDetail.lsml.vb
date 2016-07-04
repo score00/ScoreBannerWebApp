4160396c81ded1b04236251e917d0987b4e81673
scorebanner <scorebanner@gmail.com>
2016-07-04 00:45:14 -0400

    Add project files.

---
+++ ScoreBannerWebApp/ScoreBannerWebApp.DesktopClient/Screens/CustomerDetail.lsml.vb
@@ -0,0 +1,23 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class CustomerDetail
+
+        Private Sub Customer_Loaded(succeeded As Boolean)
+            ' Write your code here.
+            Me.SetDisplayNameFromEntity(Me.Customer)
+        End Sub
+
+        Private Sub Customer_Changed()
+            ' Write your code here.
+            Me.SetDisplayNameFromEntity(Me.Customer)
+        End Sub
+
+        Private Sub CustomerDetail_Saved()
+            ' Write your code here.
+            Me.SetDisplayNameFromEntity(Me.Customer)
+        End Sub
+
+    End Class
+
+End Namespace
